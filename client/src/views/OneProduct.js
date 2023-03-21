dce79db8835b08e065f29cdf03afb1cb3ba9e886
Zach Carrera <zachcarrera@gmail.com>
2023-03-21 12:55:14 -0700

    add delete button to OneProduct view

--- client/src/views/OneProduct.js
+++ client/src/views/OneProduct.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const OneProduct = () => {
     const [product, setProduct] = useState(null);
     const { _id } = useParams();
+    const navigate = useNavigate();
     useEffect(() => {
         axios
             .get(`http://localhost:8000/api/products/${_id}`)
@@ -16,6 +17,17 @@ export const OneProduct = () => {
                 console.log(error);
             });
     }, [_id]);
+
+    const deleteProduct = () => {
+        axios
+            .delete(`http://localhost:8000/api/products/${_id}`)
+            .then((res) => {
+                navigate("/");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     if (product === null) {
         return <p>Loading...</p>;
@@ -28,6 +40,7 @@ export const OneProduct = () => {
             <h2>{title}</h2>
             <p>Price: ${price}</p>
             <p>{description}</p>
+            <button onClick={() => deleteProduct()}>Delete</button>
         </div>
     );
 };
