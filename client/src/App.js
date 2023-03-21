973a5a5b59bb4ba7b78b79b3618c49edc1f799d3
Zach Carrera <zachcarrera@gmail.com>
2023-03-21 12:56:11 -0700

    add route in App.js for EditProduct view

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Dashboard } from "./views/Dashboard";
 import { OneProduct } from "./views/OneProduct";
+import { EditProduct } from "./views/EditProduct";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/:_id" element={<OneProduct />} />
+                <Route path="/:_id/edit" element={<EditProduct />} />
             </Routes>
         </div>
     );
