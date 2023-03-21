0973d948ce5e74caaee4467ab6385e62b3bd5234
Zach Carrera <zachcarrera@gmail.com>
2023-03-20 20:53:19 -0700

    add getAllProducts and getOneProduct to products.controller.js

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,6 +1,7 @@
 const { Product } = require("../models/products.model");
 
 const createProduct = (req, res) => {
+    console.log("controller: createProduct", req.body);
     Product.create(req.body)
         .then((newlyCreatedProduct) => {
             res.json({ product: newlyCreatedProduct });
@@ -10,6 +11,30 @@ const createProduct = (req, res) => {
         });
 };
 
+const getAllProducts = (req, res) => {
+    console.log("controller: getAllProducts");
+    Product.find({})
+        .then((allProducts) => {
+            res.json({ products: allProducts });
+        })
+        .catch((error) => {
+            res.json({ ...error, message: error.message });
+        });
+};
+
+const getOneProduct = (req, res) => {
+    console.log("controller: getOneProduct", req.params);
+    Product.findOne({ _id: req.params._id })
+        .then((oneProduct) => {
+            res.json({ product: oneProduct });
+        })
+        .catch((error) => {
+            res.json({ ...error, message: error.message });
+        });
+};
+
 module.exports = {
     createProduct,
+    getAllProducts,
+    getOneProduct,
 };
