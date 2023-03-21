9f1d4a53601c7ae3e535e51bfb6f4dd9f5636f57
Zach Carrera <zachcarrera@gmail.com>
2023-03-20 20:59:32 -0700

    add route for OneProduct

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Dashboard } from "./views/Dashboard";
+import { OneProduct } from "./views/OneProduct";
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/:_id" element={<OneProduct />} />
             </Routes>
         </div>
     );
