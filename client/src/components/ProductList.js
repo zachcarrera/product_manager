c93db29df3ecb0bfe4e110083c53325ffcee1b1e
Zach Carrera <zachcarrera@gmail.com>
2023-03-21 17:29:53 -0700

    update ProductList component so it will rerender without refreshes

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import { DeleteButton } from "./DeleteButton";
 
-export const ProductList = (props) => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        console.log("running useEffect from ProductList");
-        axios
-            .get("http://localhost:8000/api/products")
-            .then((res) => {
-                setProducts(res.data.products);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-
-    const addProduct = (newProduct) => {
-        setProducts([...products, newProduct]);
-    };
-
+export const ProductList = ({ products, handleDelete }) => {
     return (
-        <div>
+        <div className="w-50 mx-auto">
             <h1>All Products</h1>
             {products.map((product) => (
-                <p key={product._id}>
-                    <Link to={`/${product._id}`}>{product.title}</Link>
-                </p>
+                <div
+                    key={product._id}
+                    className="shadow mb-4 rounded border p-4"
+                >
+                    <p>
+                        <Link className="h3" to={`/${product._id}`}>
+                            {product.title}
+                        </Link>
+                    </p>
+
+                    <Link
+                        className="btn btn-warning mx-1"
+                        to={`/${product._id}/edit`}
+                    >
+                        Edit
+                    </Link>
+
+                    <DeleteButton
+                        handleClick={() => handleDelete(product._id)}
+                    />
+                </div>
             ))}
         </div>
     );
