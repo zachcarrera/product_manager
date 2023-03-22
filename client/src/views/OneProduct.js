aa0ff3028ff14dbe165792db1ceffad799a03bdc
Zach Carrera <zachcarrera@gmail.com>
2023-03-21 17:30:59 -0700

    add DeleteButton component and bootstrap styling

--- client/src/views/OneProduct.js
+++ client/src/views/OneProduct.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { DeleteButton } from "../components/DeleteButton";
 
 export const OneProduct = () => {
     const [product, setProduct] = useState(null);
@@ -36,11 +37,11 @@ export const OneProduct = () => {
     const { title, price, description } = product;
 
     return (
-        <div>
+        <div className="mt-5 w-50 rounded shadow border mx-auto p-3">
             <h2>{title}</h2>
             <p>Price: ${price}</p>
             <p>{description}</p>
-            <button onClick={() => deleteProduct()}>Delete</button>
+            <DeleteButton handleClick={() => deleteProduct()} />
         </div>
     );
 };
